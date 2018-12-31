6b549c3611a24d8d7f3a15144145d2ffc0e9b49b
Brandon White <mpcomputertech1@gmail.com>
2018-12-31 16:19:49 -0700

    generator empty attribute tests

--- src/tests/GeneratorTest.php
+++ src/tests/GeneratorTest.php
@@ -3,9 +3,8 @@ declare(strict_types=1);
 namespace Ucsp\Test;
 
 chdir(__DIR__);
-define("PROJECT_PATH", __DIR__);
-require_once(PROJECT_PATH.'/../includes/custom-exceptions.php');
 
+require_once(PROJECT_PATH.'/../includes/custom-exceptions.php');
 
 use PHPUnit\Framework\TestCase;
 use \Ucsp\Generator;
@@ -14,18 +13,64 @@ class GeneratorTest extends TestCase {
   protected function setUp() {
     $this->Generator = new Generator();
   }
+
   protected function tearDown() {
     unset($this->Generator);
   }
 
+  public function nonMatchingCustomAttributeProvider() {
+      return [
+          "isNull" => [null],
+          "empty array" => [[]],
+          "with some attributes" => [[['id' => 1, 'name' => 'Stripe Customer ID', 'key' => 'stripeCustomerId', 'attributeType' => 'client'], ['id' => 3, 'name' => 'test', 'key' => 'test', 'attributeType' => 'invoice']]]
+      ];
+  }
+
+  public function customAttributeProvider() {
+    return [
+      [[
+        ['id' => 1, 'name' => 'Ucsp Stripe User Id', 'key' => 'UcspStripeUserId', 'attributeType' => 'client'],
+        ['id' => 2, 'name' => 'Ucsp Form Email', 'key' => 'UcspFormEmail', 'attributeType' => 'client'],
+        ['id' => 3, 'name' => 'Ucsp Form Step', 'key' => 'UcspFormStep', 'attributeType' => 'client'],
+        ['id' => 4, 'name' => 'Ucsp Errors', 'key' => 'UcspErrors', 'attributeType' => 'client']
+      ]]
+    ];
+  }
+
+  /**
+  * @test
+  * @dataProvider nonMatchingCustomAttributeProvider
+  **/
+  public function missingCustomAttributes($mock_results) {
+    $mock = $this->getMockBuilder(Generator::class)
+                 ->setMethods(['get'])
+                 ->getMock();
+    $mock->method('get')->will($this->returnValue($mock_results));
+
+    $this->assertFalse($mock->customAttributesExists('UcspGatewayToken'));
+  }
+
+  /**
+  * @test
+  * @dataProvider customAttributeProvider
+  **/
+  public function customAttributesExists($mock_results) {
+    $mock = new Generator();
+
+    $mock = $this->getMockBuilder(Generator::class)
+                 ->setMethods(['get'])
+                 ->getMock();
+    $mock->method('get')->will($this->returnValue($mock_results));
+
+    $this->assertTrue($mock->customAttributesExists('UcspGatewayToken'));
+  }
 
   /**
   * @test
   **/
-  public function addStripeToClientIfClientExists() {
-    $mock = $this->getMockBuilder(Generator::class)
-                 ->setMethods(['addStripe'])
-                 ->getMock();
-    $this->assertFalse($mock->isAccessGranted());
+  public function createCustomAttributes() {
   }
+
+
+
 }
--- src/ucsp/Generator.php
+++ src/ucsp/Generator.php
@@ -4,14 +4,40 @@ namespace Ucsp;
 
 chdir(__DIR__);
 define("GENERATOR_PATH", __DIR__);
-require_once(CONFIG_PATH.'/../includes/custom-exceptions.php');
+require_once(GENERATOR_PATH.'/../includes/custom-exceptions.php');
 
 class Generator {
+  private $UscpTokens = ['UcspStripeUserId', 'UcspFormEmail', 'UcspFormStep', 'UcspErrors'];
+
   public function __construct() {
+    // $this->api = new Interpreter();
     $this->api = \Ubnt\UcrmPluginSdk\Service\UcrmApi::create();
+
     // $this->config = Config::create();
   }
 
-  public function addStripeUserToClient {
+  public function get($endpoint) {
+    return $this->api->get('custom-attributes');
   }
+
+  public function customAttributesExists($attrId) {
+    $results = $this->get('custom-attributes');
+
+    if (empty($results) || is_null($results)) {
+      return false;
+    } else {
+      $keys = array_map(function($i) {
+        return $i['key'];
+      }, $results);
+
+      $remainder = count(array_diff($this->UscpTokens, $keys));
+      
+      if ($remainder > 0) {
+        return false;
+      } else {
+        return true;
+      }
+
+    }
+  }  
 }
